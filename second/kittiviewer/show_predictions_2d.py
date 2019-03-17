ce4127773eb629b74e4171d3c7bb32319b0a53c4
Fredrik00 <Fredrik@Vatsendvik.com>
2019-03-17 17:32:39 +0100

    add objectlabel class

--- second/kittiviewer/show_predictions_2d.py
+++ second/kittiviewer/show_predictions_2d.py
@@ -11,6 +11,65 @@ BOX_COLOUR_SCHEME = {
     'Cyclist': '#FFFF00'        # Yellow
 }
 
+class ObjectLabel:
+    """Object Label Class
+    1    type         Describes the type of object: 'Car', 'Van', 'Truck',
+                      'Pedestrian', 'Person_sitting', 'Cyclist', 'Tram',
+                      'Misc' or 'DontCare'
+
+    1    truncated    Float from 0 (non-truncated) to 1 (truncated), where
+                      truncated refers to the object leaving image boundaries
+
+    1    occluded     Integer (0,1,2,3) indicating occlusion state:
+                      0 = fully visible, 1 = partly occluded
+                      2 = largely occluded, 3 = unknown
+
+    1    alpha        Observation angle of object, ranging [-pi..pi]
+
+    4    bbox         2D bounding box of object in the image (0-based index):
+                      contains left, top, right, bottom pixel coordinates
+
+    3    dimensions   3D object dimensions: height, width, length (in meters)
+
+    3    location     3D object location x,y,z in camera coordinates (in meters)
+
+    1    rotation_y   Rotation ry around Y-axis in camera coordinates [-pi..pi]
+
+    1    score        Only for results: Float, indicating confidence in
+                      detection, needed for p/r curves, higher is better.
+    """
+
+    def __init__(self):
+        self.type = ""  # Type of object
+        self.truncation = 0.
+        self.occlusion = 0.
+        self.alpha = 0.
+        self.x1 = 0.
+        self.y1 = 0.
+        self.x2 = 0.
+        self.y2 = 0.
+        self.h = 0.
+        self.w = 0.
+        self.l = 0.
+        self.t = (0., 0., 0.)
+        self.ry = 0.
+        self.score = 0.
+
+    def __eq__(self, other):
+        """Compares the given object to the current ObjectLabel instance.
+
+        :param other: object to compare to this instance against
+        :return: True, if other and current instance is the same
+        """
+        if not isinstance(other, ObjectLabel):
+            return False
+
+        if self.__dict__ != other.__dict__:
+            return False
+        else:
+            return True
+
+
 def visualization(image, display=True, fig_size=(15, 9.15)):
     """Forms the plot figure and axis for the visualization
 
@@ -361,7 +420,7 @@ def main(BACKEND, image, points, calib):
     """
     annos = inference_by_input(BACKEND, points, calib, image.shape[:2])
 
-    pred_objects = [dict() for prediction in annos["labels"]]
+    pred_objects = [ObjectLabel() for prediction in annos["labels"]]
 
     # Fail if only one object?
     for i in range(len(pred_objects)):
