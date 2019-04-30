b8dfb30f378e9e8e3f19582216d56dd2bde2c988
Fredrik00 <Fredrik@Vatsendvik.com>
2019-04-30 13:21:12 +0200

    Add new functions to init

--- torchplus/train/__init__.py
+++ torchplus/train/__init__.py
@@ -1,6 +1,6 @@
-from torchplus.train.checkpoint import (latest_checkpoint, restore,
+from torchplus.train.checkpoint import (latest_checkpoint, all_checkpoints, restore,
                                         restore_latest_checkpoints,
                                         restore_models, save, save_models,
-                                        try_restore_latest_checkpoints)
+                                        try_restore_latest_checkpoints, get_name_to_model_map)
 from torchplus.train.common import create_folder
 from torchplus.train.optim import MixedPrecisionWrapper
