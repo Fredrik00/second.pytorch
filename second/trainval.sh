255c916a04d70675e4ada82101f908fa8774b21a
root <fredrik@vatsendvik.com>
2019-05-06 08:40:02 +0000

    modify trainval script

--- second/trainval.sh
+++ second/trainval.sh
@@ -1,2 +1,2 @@
 python ./pytorch/train.py train --config_path=./configs/$1.config --model_dir=/notebooks/second_models/$2
-python ./pytorch/train.py evaluate --config_path=./configs/$1.config --model_dir=/notebooks/second_models/$2 --measure_time=True --batch_size=1
+python ./pytorch/train.py evaluate --config_path=./configs/$1.config --model_dir=/notebooks/second_models/$2 --measure_time=True --batch_size=1 --eval_all=True 2>&1 | tee $2_results.txt
